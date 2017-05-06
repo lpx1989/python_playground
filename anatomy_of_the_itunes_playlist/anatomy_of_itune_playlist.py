8518da02cb29e21aea4424352acac4321f5f0c23
Liu.Peixin <liupeixin1989@gmail.com>
2017-05-05 23:42:30 -0400

    update

--- anatomy_of_the_itunes_playlist/anatomy_of_itune_playlist.py
+++ anatomy_of_the_itunes_playlist/anatomy_of_itune_playlist.py
@@ -78,6 +78,29 @@ def findCommonTracks(fileNames):
 
 
 
+def plotStats(fileName):
+	# read in a playlist
+	plist = plistlib.readPlist(fileName)
+	# get the tracks from the playlist
+	tracks = plist['Tracks']
+	# create lists of song ratings and track durations
+	ratings = []
+	durations = []
+	# iterate through the tracks
+	for trackId, track in tracks.items():
+		try:
+			ratings.append(track['Album Rating'])
+			durations.append(track['Total Time'])
+		except:
+			# ignore
+			pass
+
+	# ensure that valid data was collected
+	if ratings == [] or durations == []:
+		print("No valid Album Rating/Total Time data in %s." % fileName)
+		return
+
+
 
 def main():
 	# create parser
