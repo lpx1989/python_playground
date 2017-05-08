de5c8a252a2701d6d06b3bc7386a4c13b22f69ea
Liu.Peixin <liupeixin1989@gmail.com>
2017-05-07 23:52:10 -0400

    update file

--- anatomy_of_the_itunes_playlist/anatomy_of_itune_playlist.py
+++ anatomy_of_the_itunes_playlist/anatomy_of_itune_playlist.py
@@ -1,45 +1,4 @@
 
-def findDuplicates(fileName):
-	print('Finding Duplicate tracks in %s...' % fileName);
-	# read in a playlist
-	plist = plistlib.readPlist(fileName)
-	# get the tracks from the Tracks dictionary
-	tracks = plist['Tracks']
-	# create a track name dictionary
-	trackNames = {}
-	# iterate through the tracks
-	for trackId, track in tracks.items():
-		try:
-			name = track['Name']
-			duration = track['Total Time']
-			# look for existing entries
-			if name in trackNames:
-                # if a name and duration match, increment the count
-                # round the track length to the nearest second
-				if duration//1000 == trackNames[name][0]//1000:
-					count = trackNames[name][1]
-					trackNames[name] = (duration, count + 1) 
-			else:
-				# add dictionary entry as tuple (duration, count)
-				trackNames[name] = (duration, 1)
-		except:
-			# ignore
-			pass
-	# store duplicates as (name, count) tuples
-	dups = []
-	for k, v in trackNames.items():
-		if v[1] > 1:
-			dups.append((v[1], k))
-	# save duplicates to a file
-	if len(dups) > 0:
-		print("Found %d duplicates. Track names saved to dup.txt" % len(dups))
-	else:
-		print("No duplicate tracks found!")
-	f = open("dup.txt", "w")
-	for val in dups:
-		f.write("[%d] %s\n" % (val[0], val[1]))
-	f.close()
-
 
 
 def findCommonTracks(fileNames):
@@ -107,7 +66,60 @@ def plotStats(fileName):
 	y = np.array(ratings, np.int32)
 	pyplot.subplot(2, 1, 1)
 	pyplot.plot(x, y, 'o')
-	# To do...
+	pyplot.axis([0, 1.05*np.max(x), -1, 110])
+	pyplot.xlabel('Track duration')
+	pyplot.ylabel('Track rating')
+
+	# plot histogram
+	pyplot.subplot(2, 1, 2)
+	pyplot.hist(x, bins=20)
+	pyplot.xlabel('Track duration')
+	pyplot.ylabel('Count')
+
+	# show plot
+	pyplot.show()
+
+
+def findDuplicates(fileName):
+	print('Finding Duplicate tracks in %s...' % fileName);
+	# read in a playlist
+	plist = plistlib.readPlist(fileName)
+	# get the tracks from the Tracks dictionary
+	tracks = plist['Tracks']
+	# create a track name dictionary
+	trackNames = {}
+	# iterate through the tracks
+	for trackId, track in tracks.items():
+		try:
+			name = track['Name']
+			duration = track['Total Time']
+			# look for existing entries
+			if name in trackNames:
+                # if a name and duration match, increment the count
+                # round the track length to the nearest second
+				if duration//1000 == trackNames[name][0]//1000:
+					count = trackNames[name][1]
+					trackNames[name] = (duration, count + 1) 
+			else:
+				# add dictionary entry as tuple (duration, count)
+				trackNames[name] = (duration, 1)
+		except:
+			# ignore
+			pass
+	# store duplicates as (name, count) tuples
+	dups = []
+	for k, v in trackNames.items():
+		if v[1] > 1:
+			dups.append((v[1], k))
+	# save duplicates to a file
+	if len(dups) > 0:
+		print("Found %d duplicates. Track names saved to dup.txt" % len(dups))
+	else:
+		print("No duplicate tracks found!")
+	f = open("dup.txt", "w")
+	for val in dups:
+		f.write("[%d] %s\n" % (val[0], val[1]))
+	f.close()
 
 
 
